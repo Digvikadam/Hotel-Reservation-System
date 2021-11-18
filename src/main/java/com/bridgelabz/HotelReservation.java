944e4c07dc982565d1fcc86c05cbefa5917f3451
[Digvikadam] <[digvikadam168@gmail.com]>
2021-11-18 09:35:40 +0530

    Added: Add Hotels in a Hotel Reservation System

--- src/main/java/com/bridgelabz/HotelReservation.java
+++ src/main/java/com/bridgelabz/HotelReservation.java
@@ -1,7 +1,21 @@
 package com.bridgelabz;
 
+import java.util.ArrayList;
+import java.util.List;
 public class HotelReservation {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Hotel Reservation Program");
-	}
+	private List<Hotel> hotels = new ArrayList<Hotel>();
+
+	    public static void main(String[] args) {
+	        System.out.println("Welcome to Hotel Reservation Program");
+	    }
+	    /* method to add the hotel
+	     * @return
+	     */
+	    public void add(Hotel hotel) {
+	        this.hotels.add(hotel);
+	    }
+
+	    public List<Hotel> getHotelList() {
+	        return this.hotels;
+	    }
 }
